8a0de7aad0e29c4aa0d9ba56ecd0d45f36cada4b
AmitBiradar1202 <amitbiradar1202@gmail.com>
2024-03-14 02:25:47 +0530

    FrontEnd_Update

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,51 +1,47 @@
-import React, { useContext, useEffect } from 'react'
-import {Context} from "./main.jsx"
-import Login from "./components/Auth/Login.jsx"
-import Register from './components/Auth/Register.jsx'
-import Navbar from './components/Layout/Navbar.jsx'
-import Footer from './components/Layout/Footer.jsx'
-import Home from './components/Home/Home.jsx'
-import JobDetails from './components/Job/JobDetails.jsx'
-import AllJobs from './components/Job/AllJobs.jsx'
-import MyJobs from './components/Job/MyJobs.jsx'
-import PostJob from './components/Job/PostJob.jsx'
-import MyApplication from './components/Application/MyApplication.jsx'
-import Application from './components/Application/Application.jsx'
-import Errors from './components/NotFound/Errors.jsx'
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { Context } from "./main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
-import axios from "axios"
-import {Toaster} from "react-hot-toast"
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import { Toaster } from "react-hot-toast";
+import axios from "axios";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Home from "./components/Home/Home";
+import AllJobs from "./components/Job/AllJobs";
+import JobDetails from "./components/Job/JobDetails";
+import Application from "./components/Application/Application";
+import MyApplications from "./components/Application/MyApplicationcd";
+import PostJob from "./components/Job/PostJob";
+import Errors from "./components/NotFound/Errors";
+import MyJobs from "./components/Job/MyJobs";
 
 const App = () => {
-
-  const {Authorized,setAuthorized,setUser}=useContext(Context);
-  useEffect(()=>{
-      const fetchUser =async()=>{
-        try{
-          const fetchUser= await axios.get("",{withCredentials:true})
-          setUser(response.data.user);
-          setAuthorized(true);
-
-        }
-      
-      catch(error){
-          setAuthorized(false);
+  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/getuser",
+          {
+            withCredentials: true,
+          }
+        );
+        setUser(response.data.user);
+        setIsAuthorized(true);
+      } catch (error) {
+        setIsAuthorized(false);
       }
-      }
-      fetchUser();
-  },[Authorized]) //We have written Authorized beacuse authorizedd changes 
-                  //fecthUser function Works
+    };
+    fetchUser();
+  }, [isAuthorized]);
+
   return (
-    <div>
-      <Router>
+    <>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/login" element={<Login />} />
@@ -54,16 +50,16 @@ const App = () => {
           <Route path="/job/getall" element={<AllJobs />} />
           <Route path="/job/:id" element={<JobDetails />} />
           <Route path="/application/:id" element={<Application />} />
-          <Route path="/applications/me" element={<MyApplication />} />
+          <Route path="/applications/me" element={<MyApplications />} />
           <Route path="/job/post" element={<PostJob />} />
           <Route path="/job/me" element={<MyJobs />} />
           <Route path="*" element={<Errors />} />
         </Routes>
-        <Footer/>
+        <Footer />
         <Toaster />
-      </Router>
-    </div>
-  )
-}
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
